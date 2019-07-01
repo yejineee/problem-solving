06e37050766cfae675fbe15ec1890b101e7c5d5d
yejineee <21700431@handong.edu>
2019-07-01 17:27:15 +0900

    카드 - 수정 : vector 사용하지 않은 것으로
    Signed-off-by: yejineee <21700431@handong.edu>

--- sort/11652.cpp
+++ sort/11652.cpp
@@ -6,40 +6,34 @@
 using namespace std ; 
 #define CARD long long
 #define COUNT long long
-typedef vector<pair<CARD, COUNT> > MIN;
-map<CARD, COUNT> mymap;
-MIN min_v ;  
-vector<pair<CARD, COUNT> >::iterator it ; 
-struct myclass{
-  bool operator()(pair<CARD, COUNT> a, pair<CARD, COUNT> b){
-    return (a.first < b.first) ; 
-  }
-}mysort ;
+typedef pair<CARD, COUNT> MIN;
+
 int main(){
+  map<CARD, COUNT> mymap;
+  MIN min_v = make_pair(-1,-1);
   int N ;
   long long in ;
   scanf("%d",&N) ; 
   while(N--){
     scanf("%lld",&in) ; 
-    if(mymap.find(in) == mymap.end()){//initial
+    if(mymap.find(in) == mymap.end()){
       mymap[in] = 1 ; 
     }else{
       mymap[in]++ ; 
     }
-    if(!min_v.size()){
-      min_v.push_back(make_pair(in, mymap[in])) ;
+    if(min_v.first == -1){
+      min_v = make_pair(in, mymap[in]) ;
     }
-    else if(min_v.front().second < mymap[in]){
-      min_v.clear();
-      min_v.push_back(make_pair(in, mymap[in])) ; 
+    else if(min_v.second < mymap[in]){
+      MIN tmp = make_pair(in, mymap[in]) ;
+      min_v.swap(tmp); 
     }
-    else if(min_v.front().second == mymap[in]){
-      min_v.push_back(make_pair(in, mymap[in])) ;
+    else if(min_v.second == mymap[in] && min_v.first > in){
+      MIN tmp = make_pair(in, mymap[in]) ;
+      min_v.swap(tmp); 
     }
   }
 
-  long long ret = min_v.front().first ; 
-  sort(min_v.begin(), min_v.end(), mysort) ; 
-  printf("%lld\n",min_v.front().first) ;
+  printf("%lld\n",min_v.first) ;
   return 0 ;
 }
