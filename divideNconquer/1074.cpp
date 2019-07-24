2cdfe782a7e4e9b824b03b9c751cc08f0c369c2b
yejineee <21700431@handong.edu>
2019-07-25 02:07:24 +0900

    2019-07-24 Z
    Signed-off-by: yejineee <21700431@handong.edu>

---
+++ divideNconquer/1074.cpp
@@ -0,0 +1,39 @@
+
+
+#include <iostream>
+#include <cmath>
+
+int N, R, C ;
+
+int 
+divide (int r, int c, int len){
+  if(len < 1) return 0 ;
+  
+  int half_len = len / 2 ;
+  if( r < half_len && c < half_len){     // 1
+    return divide(r, c, half_len) ;
+  }
+  else if(r < half_len && c >= half_len){ // 2
+	  return pow(half_len, 2) + divide(r, c-half_len, half_len) ;
+  }
+  else if(r >= half_len && c < half_len){ // 3
+    return pow(half_len, 2) * 2 + divide(r-half_len, c, half_len) ;
+  }
+  else if(r >= half_len && c >= half_len){ // 4
+    return pow(half_len, 2) * 3 + divide(r-half_len, c-half_len , half_len) ;
+  }
+  
+}
+
+int 
+main (void) {
+
+  scanf("%d %d %d", &N, &R, &C) ;
+  printf("%d\n",divide(R, C, pow(2,N) ) ) ;
+
+}
+
+
+
+
+
