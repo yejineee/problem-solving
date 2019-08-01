09f8bd8621addacbeb1d07cee73ed879d544219b
yejineee <21700431@handong.edu>
2019-08-01 15:50:54 +0900

    2019-08-01 내려가기 : 메모리 더 적게 사용
    Signed-off-by: yejineee <21700431@handong.edu>

--- dynamic_programming/2096.cpp
+++ dynamic_programming/2096.cpp
@@ -3,79 +3,51 @@
 #include <array>
 using namespace std ;
 
-int score[100000][3] ;
-array<int, 3> pre_table ;
-array<int, 3> post_table ;
+array<int, 3> score ;
+array<int, 3> min_table ;
+array<int, 3> max_table ;
+array<int, 3> max_tmp ;
+array<int, 3> min_tmp ;
 int MAX_S = 0 ;
 int MIN_S = 999999 ;
 int N ;
 
-void
-fill_table(int n, array<int, 3> &table){
-  for(int x = 0 ; x < 3 ; x++){
-    table[x] = n ;
-  }
-}
-void 
-find_min (){
-  int y = 1 ;
-  int cur_score ;
-  while(y < N){
-    for(int x = 0 ; x < 3 ; x++){
-      cur_score = score[y][x] ;
-      post_table[x] = min(post_table[x], pre_table[x] + cur_score ) ;
-      if(x-1 >= 0) post_table[x] = min(post_table[x], pre_table[x-1] + cur_score ) ;
-      if(x+1 < 3)  post_table[x] = min(post_table[x], pre_table[x+1] + cur_score ) ;
-    }
-    y += 1 ;
-    pre_table.swap(post_table) ;
-    fill_table(MIN_S, post_table) ;
-  }
-  for(int x = 0 ; x < 3 ; x++){
-    MIN_S = min(MIN_S,pre_table[x]) ;
-  }
-}
-
-void 
-find_max (){
-  int y = 1 ;
-  int cur_score ;
-  while(y < N){
-    for(int x = 0 ; x < 3 ; x++){
-      cur_score = score[y][x] ;
-      post_table[x] = max(post_table[x], pre_table[x] + cur_score ) ;
-      if(x-1 >= 0) post_table[x] = max(post_table[x], pre_table[x-1] + cur_score ) ;
-      if(x+1 < 3)  post_table[x] = max(post_table[x], pre_table[x+1] + cur_score ) ;
-    }
-    y += 1 ;
-    pre_table.swap(post_table) ;
-    fill_table(MAX_S, post_table) ;
-  }
-  for(int x = 0 ; x < 3 ; x++){
-    MAX_S = max(MAX_S,pre_table[x]) ;
-  }
-}
 
 int 
 main () {
   scanf("%d", &N) ;
   for(int y = 0 ; y < N ; y++){
     for(int x = 0 ; x < 3 ; x++){
-      scanf("%d", &score[y][x]) ;
+      scanf("%d", &score[x]) ;
+    }
+    if(y == 0){
+      for(int x = 0 ; x < 3 ; x++){
+        min_tmp[x] = score[x] ;
+        max_tmp[x] = score[x] ;
+      }
+    }
+    else{
+      for(int x = 0 ; x < 3 ; x++){
+        min_table[x] = min(MIN_S, min_tmp[x] + score[x]) ;
+        max_table[x] = max(MAX_S, max_tmp[x] + score[x]) ;
+
+        if(x+1 < 3) {
+          min_table[x] = min(min_table[x], min_tmp[x+1] + score[x]) ;
+          max_table[x] = max(max_table[x], max_tmp[x+1] + score[x]) ;
+        }
+        if(x-1 >= 0) { 
+          min_table[x] = min(min_table[x], min_tmp[x-1] + score[x]) ;
+          max_table[x] = max(max_table[x], max_tmp[x-1] + score[x]) ;
+        }
+      }
+        min_table.swap(min_tmp) ;
+        max_table.swap(max_tmp) ;
     }
   }
   for(int x = 0 ; x < 3 ; x++){
-    pre_table[x] = score[0][x] ;
+    MIN_S = min(MIN_S, min_tmp[x]) ;
+    MAX_S = max(MAX_S, max_tmp[x]) ;
   }
-  fill_table(MIN_S, post_table) ;
-  find_min() ;
-
-  for(int x = 0 ; x < 3 ; x++){
-    pre_table[x] = score[0][x] ;
-  }
-  fill_table(MAX_S, post_table) ;
-  find_max() ;
-
   printf("%d %d\n", MAX_S, MIN_S) ;
 
 }
