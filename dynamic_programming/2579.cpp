7491bf40453f9a08e72c2f9567d3b12f9f03ea4f
yejineee <21700431@handong.edu>
2019-12-28 22:29:47 +0900

    계단 오르기 (2019-12-28)
    Signed-off-by: yejineee <21700431@handong.edu>

--- dynamic_programming/2579.cpp
+++ dynamic_programming/2579.cpp
@@ -1,47 +1,43 @@
 #include <iostream>
-#include <utility>
 #include <algorithm>
 using namespace std ; 
 
-int max_s ;
+const int MAX_N = 300 ; 
+int stair[MAX_N+1] ;
+int sum[3][MAX_N+1] ; // [0]: O-O, [1]: X-O, [2]: O-X-O
 int N ;
-int stair[301] ; 
-pair<int, int> table[301] ;
 
+void go_up_stair(){
+  for(int i = 0 ; i <= 2 ; i++){
+    sum[i][N] = stair[N] ; 
+  }
+  sum[0][N-1] = sum[0][N] + stair[N-1] ; 
+  sum[1][N-1] = sum[1][N] ;
+  sum[2][N-1] = sum[2][N] ;
 
-void 
-dp (int n, int flag, int sum){
-  int ret ;
-  if(n < 1){
-    max_s = max(max_s, sum) ;
-    return ;
+  for(int i = N-1 ; i >= 2 ; i--){
+    if(i == N-1){
+      sum[2][i-1] = sum[2][i] + stair[i-1] ;
+    }
+    else{
+      sum[0][i-1] = max(sum[0][i-1], sum[2][i] + stair[i-1]) ;
+    }
+    sum[1][i-1] = max(sum[1][i-1], sum[0][i]) ; 
+    sum[1][i-1] = max(sum[1][i-1], sum[2][i]) ; 
+    sum[2][i-1] = max(sum[2][i-1], sum[1][i] + stair[i-1]) ; 
   }
-  if(flag == 1){
-    //table[n].first = stair[n] + sum ;
-    ret = stair[n] + sum ;
-    if(ret < table[n].first) return ;
-    table[n].first = ret ; 
-    dp(n-2, 2, table[n].first) ;
-  }
-  else if(flag == 2){
-    //table[n].second = stair[n] + sum ;  
-    ret = stair[n] + sum ;  
-    if(ret < table[n].second) return ;
-    table[n].second = ret ; 
-    dp(n-1, 1, table[n].second ) ;
-    dp(n-2, 2, table[n].second ) ;  
-  }
-
 }
 
-int 
-main () {
+int main(){
+  int result[3] ;
   scanf("%d", &N) ; 
-  for(int i = 1 ; i <= N ; i++){
-    scanf("%d",&stair[i]) ; 
+  for(int i = 1; i <= N ; i++){
+    scanf("%d", &stair[i]) ; 
   }
-
-  dp(N-1, 1, stair[N]) ;
-  dp(N-2, 2, stair[N]) ;
-  printf("%d\n", max_s ) ; 
+  go_up_stair() ; 
+  for(int i = 0 ; i <= 2 ; i++){
+    result[i] = sum[i][1] ; 
+  }
+  printf("%d", *max_element(result, result+3)) ;
+  return 0 ;
 }
