0768a025686d476078c12d7b39acee03f8e4fc04
yejineee <yygeniee@gmail.com>
2020-12-22 09:39:50 +0900

    Add 크레인 인형 뽑기 - 2020/12/22

---
+++ 2019-kakao-winter-internship/크레인인형뽑기.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> basket;
+int nLine;
+int nPopped;
+
+
+void pushOrPopToBasket(vector<vector<int>> board, int toy){
+  if(!basket.empty() && basket.back() == toy){
+    basket.pop_back();
+    nPopped += 2;
+  }else{
+    basket.push_back(toy);
+  }
+}
+
+void moveAndPopToy(int column, vector<vector<int>> &board){
+  for(int r = 0; r < nLine; r++){
+    if(board[r][column] == 0){
+      continue;
+    }
+    pushOrPopToBasket(board, board[r][column]);
+    board[r][column] = 0;
+    break;
+  }
+}
+
+
+int solution(vector<vector<int>> board, vector<int> moves){
+  nLine = board.size();
+  int nMove = moves.size();
+  for(int i = 0; i < nMove; i++){
+    int targetColumn = moves[i]-1;
+    moveAndPopToy(targetColumn, board);
+  }
+
+  return nPopped;
+}
+
